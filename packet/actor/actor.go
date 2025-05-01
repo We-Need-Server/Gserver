655f3eeca61f444afb4f9b0e7e9ba6da08b25f6c
SeoGeonhyuk <sgh4123@naver.com>
2025-05-01 22:02:07 +0900

    refactor: change endian format

--- packet/actor/actor.go
+++ packet/actor/actor.go
@@ -25,14 +25,14 @@ func NewPacketActor(NextSEQ uint32, QPort uint32, UserAddr *net.UDPAddr, packetC
 func (a *PacketActor) ProcessLoopPacket() {
 	for {
 		pkt := <-a.packetChan
-		//fmt.Println("시퀀스 번호 전", a.NextSEQ, pkt.SEQ)
+		fmt.Println("시퀀스 번호 전", a.NextSEQ, pkt.SEQ)
 		if pkt.SEQ == a.NextSEQ {
 			a.NextSEQ += uint32(1)
 			a.processCommandPayload(pkt.Payload, pkt.PayloadEndpoint)
 		}
 
 		// 패킷 정보 출력
-		//fmt.Println("시퀀스 번호 후", a.NextSEQ, pkt.SEQ)
+		fmt.Println("시퀀스 번호 후", a.NextSEQ, pkt.SEQ)
 		fmt.Printf("패킷 수신 - 사용자: %s, QPort: %d\n", a.UserAddr, a.QPort)
 		fmt.Printf("패킷 내용: %+v\n", pkt)
 	}
--- packet/packet.go
+++ packet/packet.go
@@ -28,10 +28,10 @@ func NewPacket(SEQ uint32, ACK uint32, QPort uint32, PKind uint32, Payload []byt
 }
 
 func ParsePacket(np []byte, endPoint int) *Packet {
-	PKind := binary.BigEndian.Uint32(np[0:4])
-	SEQ := binary.BigEndian.Uint32(np[4:8])
-	ACK := binary.BigEndian.Uint32(np[8:12])
-	QPort := binary.BigEndian.Uint32(np[12:16])
+	PKind := binary.LittleEndian.Uint32(np[0:4])
+	SEQ := binary.LittleEndian.Uint32(np[4:8])
+	ACK := binary.LittleEndian.Uint32(np[8:12])
+	QPort := binary.LittleEndian.Uint32(np[12:16])
 	Payload := np[16:endPoint]
 	PayloadEndpoint := endPoint - 16
 	fmt.Println(PKind, SEQ, ACK, QPort, Payload, PayloadEndpoint)
