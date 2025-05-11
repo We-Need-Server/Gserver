add48838293c4280d2aa353206885314d99a91d4
SeoGeonhyuk <sgh4123@naver.com>
2025-05-11 16:48:43 +0900

    feat: implementation mediator

--- mediator/mediator.go
+++ mediator/mediator.go
@@ -1,1 +1,34 @@
 package mediator
+
+import "fmt"
+
+type Component interface {
+	Register(m *Mediator)
+	Receive(senderName string, message interface{})
+}
+
+type Interface interface {
+	Register()
+	Notify()
+}
+
+type Mediator struct {
+	components map[string]Component
+}
+
+func NewMediator() *Mediator {
+	return &Mediator{make(map[string]Component)}
+}
+
+func (m *Mediator) Register(name string, component Component) (string, error) {
+	if _, exists := m.components[name]; exists {
+		return name, fmt.Errorf("already existed component")
+	}
+	m.components[name] = component
+	component.Register(m)
+	return name, nil
+}
+
+func (m *Mediator) Notify(senderName string, receiverName string, message interface{}) {
+	m.components[receiverName].Receive(senderName, message)
+}
