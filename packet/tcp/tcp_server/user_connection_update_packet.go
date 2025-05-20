1fc66f68ed89f05264ed9a3297ee28a78872606f
SeoGeonhyuk <sgh4123@naver.com>
2025-05-20 14:25:16 +0900

    feat: implementation round start packet

--- packet/tcp/tcp_server/user_connection_update_packet.go
+++ packet/tcp/tcp_server/user_connection_update_packet.go
@@ -1,13 +1,13 @@
 package tcp_server
 
 type UserConnectionUpdatePacket struct {
-	PacketKind uint8    `json:"packetKind"`
-	UserList   []uint32 `json:"userList"`
+	PKind    uint8    `json:"packetKind"`
+	UserList []uint32 `json:"userList"`
 }
 
 func NewUserConnectionUpdatePacket(userList []uint32) *UserConnectionUpdatePacket {
 	return &UserConnectionUpdatePacket{
-		PacketKind: 'U',
-		UserList:   userList,
+		PKind:    'U',
+		UserList: userList,
 	}
 }
