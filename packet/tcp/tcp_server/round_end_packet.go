910ba2c699f38578762ca977b2f8b80fe18fa5d6
SeoGeonhyuk <sgh4123@naver.com>
2025-05-20 14:58:14 +0900

    feat: implemenation round end packet

---
+++ packet/tcp/tcp_server/round_end_packet.go
@@ -0,0 +1,17 @@
+package tcp_server
+
+type RoundEndPacket struct {
+	PKind      uint8  `json:"packetKind"`
+	WinnerTeam uint8  `json:"winnerTeam"`
+	RedScore   uint16 `json:"redScore"`
+	BlueScore  uint16 `json:"blueScore"`
+}
+
+func NewRoundEndPacket(winnerTeam uint8, redScore uint16, blueScore uint16) *RoundEndPacket {
+	return &RoundEndPacket{
+		PKind:      'E',
+		WinnerTeam: winnerTeam,
+		RedScore:   redScore,
+		BlueScore:  blueScore,
+	}
+}
